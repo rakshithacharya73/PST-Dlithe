4bf105939c9ee336a73b5e164849824266fbcfe5
rakshithacharya73 <162448019+rakshithacharya73@users.noreply.github.com>
2026-06-09 19:23:43 +0530

    10

---
+++ PST-DSA/Assignment/Day10/Day10_Assignment.sql
@@ -0,0 +1,49 @@
+create database assignment;
+use assignment;
+CREATE TABLE Customer (
+    CustomerID VARCHAR(10) PRIMARY KEY,
+    CustomerName VARCHAR(50),
+    DOB DATE,
+    ContactNo VARCHAR(15)
+);
+
+CREATE TABLE PolicyType (
+    PolicyType VARCHAR(20) PRIMARY KEY,
+    PremiumRate DECIMAL(5,2),
+    CoverageYears INT
+);
+
+CREATE TABLE Branch (
+    BranchID VARCHAR(10) PRIMARY KEY,
+    BranchName VARCHAR(50),
+    Manager VARCHAR(50)
+);
+
+CREATE TABLE Policy (
+    PolicyID VARCHAR(10) PRIMARY KEY,
+    CustomerID VARCHAR(10),
+    PolicyType VARCHAR(20),
+    PremiumAmount DECIMAL(10,2),
+    BranchID VARCHAR(10),
+    FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID),
+    FOREIGN KEY (PolicyType) REFERENCES PolicyType(PolicyType),
+    FOREIGN KEY (BranchID) REFERENCES Branch(BranchID)
+);
+
+INSERT INTO Customer VALUES
+('C1', 'Ramesh', '1985-05-10', '9876543210'),
+('C2', 'Kavya', '1990-09-15', '9123456789');
+
+INSERT INTO PolicyType VALUES
+('Health', 5.00, 10),
+('Life', 8.00, 20),
+('Vehicle', 6.00, 5);
+
+INSERT INTO Branch VALUES
+('B1', 'Bangalore', 'Anil'),
+('B2', 'Mysore', 'Sneha');
+
+INSERT INTO Policy VALUES
+('P1', 'C1', 'Health', 20000, 'B1'),
+('P2', 'C1', 'Vehicle', 15000, 'B1'),
+('P3', 'C2', 'Life', 50000, 'B2');
